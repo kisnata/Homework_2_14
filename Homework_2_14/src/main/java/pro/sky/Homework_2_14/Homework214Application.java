ea10a86edb1a1a33a132f1b1f44867b8fb2b6dfb
Ната <nata.van4inova@yandex.ru>
2023-06-22 20:28:35 +0300

    Initial commit

---
+++ Homework_2_14/src/main/java/pro/sky/Homework_2_14/Homework214Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework_2_14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework214Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework214Application.class, args);
+	}
+
+}
---
+++ Homework_2_14/src/test/java/pro/sky/Homework_2_14/Homework214ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework_2_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework214ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
